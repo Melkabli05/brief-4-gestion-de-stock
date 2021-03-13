45ba68a9f3e2d1812303d6fe8986bcfd0ebacc0e
Mohammed El-kabli <melkabli05@gmail.com>
2021-03-13 14:26:22 +0100

    final reponsive dashboard version

--- web/main.js
+++ web/main.js
@@ -11,3 +11,23 @@ menuIconEl.addEventListener('click', function () {
 sidenavCloseEl.addEventListener('click', function () {
   sidenavEl.classList.remove('active');
 });
+
+document.querySelector("body > div > main > div.main-cards > div > table > tbody > tr:nth-child(2) > td.edit > div > a:nth-child(2)").addEventListener("click", myFunction);
+function myFunction(){
+  swal({
+    title: "Are you sure?",
+    text: "Once deleted, you will not be able to recover this imaginary file!",
+    icon: "warning",
+    buttons: true,
+    dangerMode: true,
+  })
+  .then((willDelete) => {
+    if (willDelete) {
+      swal("Poof! Your imaginary file has been deleted!", {
+        icon: "success",
+      });
+    } else {
+      swal("Your imaginary file is safe!");
+    }
+  });
+}
